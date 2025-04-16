3836600f316c4ce26cf9dd63413b9cbff7a77240
kplichta <konrad.plichta@sim.com.pl>
2025-04-16 09:53:12 +0200

    feat/ create "like" algorithm

--- test-client/src/views/home/index.tsx
+++ test-client/src/views/home/index.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./index.css";
+import { likes } from "./functions/likes";
 
 const HomeView = (): React.JSX.Element => {
+  const examples = [
+    [],
+    ["Peter"],
+    ["Jacob", "Alex"],
+    ["Max", "John", "Mark"],
+    ["Alex", "Jacob", "Mark", "Max"],
+  ];
+
   return (
-    <>
-      <div className="container">
-        <h1>Home</h1>
-        <div className="links-container">
-          <Link to="/login">Login</Link>
-        </div>
+    <div className="container">
+      <h1>Home</h1>
+      <div className="links-container">
+        <Link to="/login">Login</Link>
       </div>
-    </>
+      <div className="likes-examples">
+        {examples.map((names, index) => (
+          <p key={index}>{likes(names)}</p>
+        ))}
+      </div>
+    </div>
   );
 };
 
